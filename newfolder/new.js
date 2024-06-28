258081242536edab31642478e679db788a416271
Noah Armbruster <namrbruster@wpi.edu>
2024-06-28 10:18:57 -0400

    hrgh

--- newfolder/new.js
+++ newfolder/new.js
@@ -5,8 +5,10 @@ const text2=document.getElementById("text2");
 const text3=document.getElementById("text3");
 const text4=document.getElementById("text4");
 const text5=document.getElementById("text5");
+const winner=document.getElementById("win");
+
 let guess;
-let num;
+
 let word = "NATHAN";
 function clickButton(){
     guess=guessuh.value;
@@ -14,21 +16,23 @@ function clickButton(){
         checkGuess(guess);
     }
 }
+let c=0;
 
 
 function checkGuess(){
     for(let i=0;i<word.length;i++){
         if(guess==word[i]){
-            num=i;
+            c+=1;
+            let num=i;
             console.log(num);
             if(num==0){
-            num=text0;
+                 num=text0;
             updateScreen(num);
             }else if (num==1){
-            num=text1;
+                num=text1;
             updateScreen(num);
             }else if (num==2){
-                num=text2;
+               num=text2;
                 updateScreen(num);
             }else if (num==3){
                 num=text3;
@@ -42,13 +46,16 @@ function checkGuess(){
             }
         }
     }
+    if (c>=6){
+        winner.innerHTML = "You Win!";
+        }
 }
 
 
 
-function updateScreen(num){
+function updateScreen(n){
     guess=guess.toString();
-    num.innerHTML = guess;
+    n.innerHTML = guess;
 
     
 }
